dd7c2dbd41252af77c1a453faee2f8ce7233407d
Arpit Kashyap <arpitkashyap2000@gmail.com>
2024-11-24 07:53:18 +0530

    backend revamp

--- index.js
+++ index.js
@@ -41,26 +41,95 @@ io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
     // onconnection only to the user 
-    socket.emit('message', `Welcome to chat app`);
+    socket.emit('message', buildMessage(ADMIN, "Welcome to Chat App"));
+
+    socket.on("enterRoom", ({name,room})=>{
+        //leave previous room
+        const prevRoom = getUser(socket.id)?.room;
+        if(prevRoom){
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMessage(ADMIN, `${name} has left the chat app`))
+        }
+        const user = activateUser(socket.id, name, room);
+
+        // cannot update previous room users list until after the state update in active user
+        if(prevRoom){
+            io.to(prevRoom).emit('userList',{
+                users: getUserInRoom(prevRoom)
+            })
+        }
+
+        socket.join(user.room)
+
+        // to user who joined
+
+        socket.emit('message', buildMessage(ADMIN, `You have joind the ${user.room} chat room`));
+
+        // to everyone other in room
+
+        socket.broadcast.to(user.room).emit('message', buildMessage(ADMIN, `${user.name} has joined the room`))
+
+        // update user list for room
+
+        io.to(user.room).emit('userList', {
+            users: getUserInRoom(user.room)
+        })
+
+        // update room list for everyone
+
+        io.emit('usersList', {
+            rooms: getAllActiveRooms()
+        })
+    })
+
+    // when user diconnect
+
+    socket.on('disconnect',()=>{
+        const user = getUser(socket.id);
+        exitUser(socket.id);
+        if(user){
+            io.to(user.room).emit('message', buildMessage(ADMIN,`${user.name} has left the room`));
+
+            io.to(user.room).emit('userList',{
+                users: getUserInRoom(user.room)
+            })
+
+            io.emit('usersList', {
+                rooms: getAllActiveRooms()
+            })
+        }
+
+        console.log(`User disconnected: ${socket.id}`);
+    })
 
     // on connection to all other user
-    socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined the chat app`);
+    // socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has joined the chat app`);
 
     // listning for a message event
-    socket.on("message", data =>{
-        console.log(data);
-        io.emit("message", `${socket.id.substring(0, 5)} : ${data}`)
+    socket.on("message", ({name, text}) =>{
+        const room = getUser(socket.id)?.room
+
+        if(room){
+            io.to(room).emit('message', buildMessage(name,text))
+        }
+        // io.emit("message", `${socket.id.substring(0, 5)} : ${data}`)
+
     })
 
-    // when user disconnect - to all other user
-    socket.on("disconnect", () => {
-        socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left the chat app`);
-    })
+    // // when user disconnect - to all other user
+    // socket.on("disconnect", () => {
+    //     socket.broadcast.emit('message', `${socket.id.substring(0, 5)} has left the chat app`);
+    // })
      
 
     // Listne for activity event
     socket.on("activity", (name) => {
-        socket.broadcast.emit("activity", `${name}`)
+        const room = getUser(socket.id)?.room
+
+        if(room){
+            socket.broadcast.to(room).emit('activity', name)
+        }
+        // socket.broadcast.emit("activity", `${name}`)
     })
 })
 
@@ -94,6 +163,6 @@ function getUserInRoom(room){
     return UserState.users.filter(user => user.room === room);
 }
 
-function getActiveRoom(){
+function getAllActiveRooms(){
     return Array.from(new Set(UserState.users.map(user => user.room)));
 }
